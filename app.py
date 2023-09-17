072104d0c5104cb28c3029446c9cca9999b40bd4
NouhAhmed <n.shiine13@gmail.com>
2023-09-17 14:53:55 +0000

    Added some divs, and also lis of jobs and their detail

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Nairobi, Kenya',
+    'salary': 'ksh. 250,000'
+  },
+    {
+    'id': 2,
+    'title': 'Data Engineerin',
+    'location': 'Johnsburg, SA',
+    'salary': 'ksh. 300,000'
+  },
+  {
+    'id': 3,
+    'title': 'Front-end Engineer',
+    'location': 'remote'
+    
+  },
+  {
+    'id': 4,
+    'title': 'Back-end Engineer',
+    'location': 'San-francisco USA',
+    'salary': 'ksh. 350,000'
+  }
+]
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS, company_name="Jovian")
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
